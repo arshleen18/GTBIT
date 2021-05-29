d9e59077eb2760d7369a9019c0bc2abaf4c9ed0a
arshleen18 <84401040+arshleen18@users.noreply.github.com>
2021-05-30 01:19:55 +0530

    program 2.c
    A program to find the sum of a geometric series

---
+++ program2.c
@@ -0,0 +1,18 @@
+#WAP to find the sum of a geometric series
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a, n, r;
+    float sum;
+    printf("\nEnter first number of a G.P series: ");
+    scanf("%d", &a);
+    printf("\nEnter the total number of a G.P series: ");
+    scanf("%d", &n);
+    printf("\nEnter the common ratio: ");
+    scanf("%d", &r);
+    sum = (a * (1 - pow(r,n))) / (1-r);
+    printf("\nThe sum of G.p series = %.2f", sum);
+    return 0;
+}
