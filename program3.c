e09344445a1f5b10cd4e30ad876e4142d8265d14
arshleen18 <84401040+arshleen18@users.noreply.github.com>
2021-06-10 03:54:27 +0530

    program 3
    Write a recursive program for tower of Hanoi problem

---
+++ program3.c
@@ -0,0 +1,17 @@
+#Write a recursive program for tower of Hanoi problem
+
+#include<stdio.h>
+void TOH(int n, int A, int B, int C ) 
+{
+    if (n>0) 
+    {
+        TOH(n-1, A, C, B);
+        printf("\nmove disc %d from rod %d to rod %d", n, A, C);
+        TOH(n-1, B, A, C);
+    }
+}
+int main() 
+{
+    int n = 3;
+    TOH(n,1,2,3);
+}
