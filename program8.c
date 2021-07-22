d1498b5ecac69c4b52938b3c88313660e844ef85
arshleen18 <84401040+arshleen18@users.noreply.github.com>
2021-07-22 19:18:28 +0530

    program 8
    A program for an array of records containing information of managers and workers of a company. Print all the data of managers and workers in a separate file

---
+++ program8.c
@@ -0,0 +1,34 @@
+/*Write a C program for an array of records containing information of managers and workers of a company. Print all the data of managers and workers in a separate file*/
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    FILE *fptr;
+    int i, n, empno;
+    float bpay, allow, ded;
+    char name[10];
+    fptr = fopen("data.txt", "w");
+    printf("Enter the number of employees : ");
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+    {
+        printf("\nEnter the employee number : ");
+        scanf("%d", &empno);
+        printf("\nEnter the name : ");
+        scanf("%s", &name);
+        printf("\nEnter the basic pay, allowances & deductions : ");
+        scanf("%f %f %f", &bpay, &allow, &ded);
+        fprintf(fptr, "%d %s %f %f %f \n", empno,name,bpay,allow,ded);
+    }
+	fclose(fptr);
+	fptr = fopen("data.txt", "r");
+	printf("\nEmp. No.Name\t\t Bpay\t\t Allow\t\t Ded\t\t Npay\n\n");
+	for(i = 0; i < n; i++)
+    {
+    	fscanf(fptr,"%d%s%f%f%f\n", &empno,&name,&bpay,&allow,&ded);
+    	printf("%d \t %s \t %.2f \t %.2f \t %.2f \t %.2f \n", empno, name, bpay, allow, ded, bpay + allow - ded);
+    }
+    fclose(fptr);
+    getch();
+}
